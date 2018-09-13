d8aab45e3c3e0212c2aaf4db4c73aba2f4795132
Shane Fisher <sjfisher33@gmail.com>
2018-09-13 16:11:14 -0500

    Update RubySorts.rb

--- shane_fisher/fundamentals/RubySorts.rb
+++ shane_fisher/fundamentals/RubySorts.rb
@@ -8,3 +8,26 @@ def selectionSort(arr)
 end
 
 selectionSort([25,8,12,43,96,103,4])
+
+
+def bubblesort(arr)
+  arr.size.times do
+    arr.each_with_index do |num, index|
+      swapped = 0
+      if num == arr.last
+        next
+      else
+        if arr[index] > arr[index + 1]
+          swapped = arr[index + 1]
+          arr[index + 1] = arr[index]
+          arr[index] = swapped
+        end
+      end
+    end
+  end
+  return arr
+end
+
+arr = [16,43,102,103,101,3,1,12,56]
+
+puts bubblesort(arr).to_s
